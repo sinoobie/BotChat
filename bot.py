55d7b2b6069f0a42e452107bc2016a628bbb12b8
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-02-13 19:52:34 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,7 @@
-#!usr/bin/python
-#Author: KANG-NEWBIE
-'''
-jangan nakalin aku kak, aku masih smp:'(
-
-udah open source jangan di recode bangsad!
-'''
 from fbchat import Client
 from fbchat.models import *
 from os import system
-
+import time
 g=('\033[1;32m')
 c=('\033[1;36m')
 w=('\033[1;37m')
@@ -23,25 +16,31 @@ for q,a in zip(Q,A):
 
 class Main_Client(Client):
 	QA=QA
-	tol=False
+	bool=False
 	def onMessage(self,author_id,message_object,thread_id=None,thread_type=ThreadType.USER,**kwargs):
 		self.markAsRead(author_id)
 		print(message_object)
 		if message_object.text==None and len(message_object.attachments)>0:
-			self.send(Message(text='''Ngak ngerti.gua djancok!!.'''),thread_id=thread_id,thread_type=thread_type)
+			self.send(Message(text='''bot tidak mengerti pesan yang berupa gambar'''),thread_id=thread_id,thread_type=thread_type)
 		else:
 			self.msg=message_object.text
 			self.msg=self.msg.lower()
 			print(self.msg)
 
 		if author_id != self.uid:
+			if 'tgl berapa sekarang' in self.msg:
+				Main_Client.bool=True
+				t=time.ctime()
+				self.send(Message(text=t),thread_id=thread_id,thread_type=thread_type)
 			if self.msg in Main_Client.QA.keys():
 				getans=Main_Client.QA.get(self.msg)
 				self.messge_send=self.send(Message(text=getans),thread_id=thread_id,thread_type=thread_type)
-				Main_Client.tol=True
+				Main_Client.bool=True
 			else:
-				Main_Client.tol=True
-				self.messge_send=self.send(Message(text='''Ngak Ngerti Kak:'('''),thread_id=thread_id,thread_type=thread_type)
+				Main_Client.bool=True
+				qus=open('q.txt','r').read()
+				res='bot tidak mengerti apa yang kamu ketik, dibawah ini adalah kata yang dapat bot mengerti:\n\n{}'.format(qus)
+				self.messge_send=self.send(Message(text=res),thread_id=thread_id,thread_type=thread_type)
 		self.markAsDelivered(author_id,self.messge_send)
 
 system('clear')
